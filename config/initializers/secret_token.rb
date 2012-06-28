29795900a6c33c420aa8b2490dcf71598761c251
Michael Woffendin <woffendm@onid.orst.edu>
2012-06-28 15:46:04 -0700

    Second commit
    *I couldn't figure out how to properly exclude the database.yml file so it's in here... sadface :(

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project1::Application.config.secret_token = '1cdcba5f7f4d8837194d4f53296cafcc4398a9e5b6cbd0ddaee8e5e4075c9dcda04b210ee6e1280ea00eb22fd6531b6b47e9f91a728fe40bcea972e360b8b7b1'
---
+++ test/unit/employee_allocation_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EmployeeAllocationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
