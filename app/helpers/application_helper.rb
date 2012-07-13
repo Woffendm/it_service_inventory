b18c680cd47d9124cc3b9d5bf6d5fba3d106bceb
Michael Woffendin <woffendm@onid.orst.edu>
2012-07-13 16:35:18 -0700

    Fixed the mysterious issue of "Undefined method sub for nil nil class"!!! *Yay Ricky!!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
   
+  def javascript(*files)
+    content_for(:head) { javascript_include_tag(*files) }
+  end
+
+  def stylesheet(*files)
+    content_for(:head) { stylesheet_link_tag(*files) }
+  end
+  
   def theme_link_tag(theme)
     # => <link href=
   end
