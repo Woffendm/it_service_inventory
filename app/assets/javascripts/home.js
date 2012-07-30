9d786f87ca27f67edfbc00a7004e2c33d05653af
Michael Woffendin <woffendm@onid.orst.edu>
2012-07-30 14:42:22 -0700

    Switched all the tabs to spaces throughout the whole app.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,64 +1,64 @@
 /**
 * This file serves two primary purposes on the home view.
 * First: dynamically populate the dropdown list of employees based on the selection made in the 
-* 				group dropdown
-*	Second: dynamically populate the search-results section of the page based on the selection made in 
-*					the employees dropdown
+*         group dropdown
+*  Second: dynamically populate the search-results section of the page based on the selection made in 
+*          the employees dropdown
 */
 
 $(document).ready(function(){
-	jQuery.ajaxSetup({
-		'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-	})
+  jQuery.ajaxSetup({
+    'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+  })
 
 
-	// Gets id of selected group, passes it to "employees" method in groups controller. Default is 0.
-	var groupSelect = $("#group_dropdown");
-	groupSelect.change(function(e) {
-		var groupId = "0";
-		if (e.target.value) {
-			groupId =  e.target.value;
-		}
-		$.ajax({
-			type: "GET",
-			url: "/groups/employees?group[id]=" + groupId,
-			success: popEmployeeDropdown
-		})
-		return true;
-	});
-	
-	
-	// Gets id of selected employee, passes it to "populate_employee_results" method in employees 	
-	// controller 
-	var employeeSelect = $("#employee_dropdown");
-	employeeSelect.change(function(e) {
-		var employeeId = "0";
-		if (e.target.value) {
-			 employeeId = e.target.value;
-		}
-		$.ajax({
-			type: "GET",
-			url: "/employees/populate_employee_results?employee[id]=" + employeeId,
-			success: popEmployeeResults
-		})
-		return true;
-	});
+  // Gets id of selected group, passes it to "employees" method in groups controller. Default is 0.
+  var groupSelect = $("#group_dropdown");
+  groupSelect.change(function(e) {
+    var groupId = "0";
+    if (e.target.value) {
+      groupId =  e.target.value;
+    }
+    $.ajax({
+      type: "GET",
+      url: "/groups/employees?group[id]=" + groupId,
+      success: popEmployeeDropdown
+    })
+    return true;
+  });
+  
+  
+  // Gets id of selected employee, passes it to "populate_employee_results" method in employees   
+  // controller 
+  var employeeSelect = $("#employee_dropdown");
+  employeeSelect.change(function(e) {
+    var employeeId = "0";
+    if (e.target.value) {
+       employeeId = e.target.value;
+    }
+    $.ajax({
+      type: "GET",
+      url: "/employees/populate_employee_results?employee[id]=" + employeeId,
+      success: popEmployeeResults
+    })
+    return true;
+  });
 });
 
 
 // Replaces the employee dropdown list on the home view with the updated list provided via the
 // "employees" method in the groups controller
 function popEmployeeDropdown(response){
-	groupSelect = $("#group")
-	$("#employee_select_div").html(response);
-	return false;
+  groupSelect = $("#group")
+  $("#employee_select_div").html(response);
+  return false;
 }
 
 
 // Populates the employee results div on the home view with relevant information provided via the 
 // "populate_employee_results" method in employees controller
 function popEmployeeResults(response){
-	employeeSelect = $("#employee")
-	$("#employee_results").html(response);
-	return false;
+  employeeSelect = $("#employee")
+  $("#employee_results").html(response);
+  return false;
 }
--- app/assets/javascripts/ldap_search_results.js
+++ app/assets/javascripts/ldap_search_results.js
@@ -6,37 +6,37 @@
 * the "ldap_search_results" page is rendered in its designated section on the "search_ldap_view"
 */
 $(document).ready(function(){
-	jQuery.ajaxSetup({
-		'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-	})
-	
-	
-	// Searches the OSU directory for everyone whose last name begins with whatever is entered in the 
-	// appropriate text box on the "search_ldap_view" provided that the entry has at least two 
-	// characters. After the user enters at least two characters for the last name field, the method 
-	// will further restrict search results by whatever is entered in the first name field. It should 
-	// be noted however that the field name of "first name" is somewhat misleading, as the first name 
-	// search will actually return both first and middle names that match the query (this is 
-	// intentional).
-	var searchDiv = $("#search_div");
-	searchDiv.keyup(function(e) {
-		var first_name = $('#first_name').val();
-		var last_name = $('#last_name').val();
-		if (last_name.length > 1) {
-			$.ajax({
-				type: "get",
-				url: "/employees/ldap_search_results?last_name=" + last_name + "&first_name=" + first_name,
-				success: popLdapSearchResults
-			})
-		}
-		return false;
-	});
+  jQuery.ajaxSetup({
+    'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+  })
+  
+  
+  // Searches the OSU directory for everyone whose last name begins with whatever is entered in the 
+  // appropriate text box on the "search_ldap_view" provided that the entry has at least two 
+  // characters. After the user enters at least two characters for the last name field, the method 
+  // will further restrict search results by whatever is entered in the first name field. It should 
+  // be noted however that the field name of "first name" is somewhat misleading, as the first name 
+  // search will actually return both first and middle names that match the query (this is 
+  // intentional).
+  var searchDiv = $("#search_div");
+  searchDiv.keyup(function(e) {
+    var first_name = $('#first_name').val();
+    var last_name = $('#last_name').val();
+    if (last_name.length > 1) {
+      $.ajax({
+        type: "get",
+        url: "/employees/ldap_search_results?last_name=" + last_name + "&first_name=" + first_name,
+        success: popLdapSearchResults
+      })
+    }
+    return false;
+  });
 });
 
 
 // Displays the search results in the appropriate div on the "search_ldap_view" page
 function popLdapSearchResults(response){
-	lastNameSearch = $("#last_name")
-	$("#ldap_search_results").html(response);
-	return false;
+  lastNameSearch = $("#last_name")
+  $("#ldap_search_results").html(response);
+  return false;
 }
