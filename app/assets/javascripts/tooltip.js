a80820322a463d84b35daabb709f211e9b7aebcc
Michael Woffendin <woffendm@onid.orst.edu>
2012-08-30 10:43:47 -0700

    Fix css issues
    Re: #21024
    
    * Add tooltips to user settings
    * Add translations for said tooltips
    * Add indentation to english translation file
    * Fix css naming
    * Add new css

--- app/assets/javascripts/tooltip.js
+++ app/assets/javascripts/tooltip.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   // .tooltip establishes the specified div as a tooltip and sets some custom positioning
   // .dynamic detects if the tooltip is displaying off the screen. If it is, then it corrects the 
   // tooltip's display so it is onscreen.
-  $("div.tooltip_trigger").tooltip({ offset : [-20, 50]
+  $(".tooltip_trigger").tooltip({ offset : [-20, 50]
                         }).dynamic({ bottom : { direction : 'down', 
                                                 bounce    : true } })
 });
