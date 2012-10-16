f8fa0349716d69db841f5a7b1597df5686e72ea2
Michael Woffendin <woffendm@onid.orst.edu>
2012-10-16 15:11:58 -0700

    Add new seeds for product state and type

---
+++ db/fixtures/product_states.rb
@@ -0,0 +1,19 @@
+ProductState.seed do |t|
+  t.name = "Alpha"
+end
+
+ProductState.seed do |t|
+  t.name = "Beta"
+end
+
+ProductState.seed do |t|
+  t.name = "Release Candidate"
+end
+
+ProductState.seed do |t|
+  t.name = "General Availability (GA)"
+end
+
+ProductState.seed do |t|
+  t.name = "Discontinued"
+end
---
+++ db/fixtures/product_types.rb
@@ -0,0 +1,19 @@
+ProductType.seed do |t|
+  t.name = "Commercial"
+end
+
+ProductType.seed do |t|
+  t.name = "Open Source"
+end
+
+ProductType.seed do |t|
+  t.name = "In-House"
+end
+
+ProductType.seed do |t|
+  t.name = "SAAS"
+end
+
+ProductType.seed do |t|
+  t.name = "PAAS"
+end
