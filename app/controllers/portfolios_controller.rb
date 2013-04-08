ddd640d8668f53e270594ab5f079d25dd4e2ddc6
Michael Woffendin <woffendm@onid.orst.edu>
2013-04-08 14:42:40 -0700

    create rough draft of editing portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,14 +2,28 @@ class PortfoliosController < ApplicationController
   before_filter :load_portfolio
   
   
-  
+  # View for editing a portfolio.
   def edit
-    @portfolio_products = @portfolio.portfolio_products.joins(:products).order(:name)
+    @portfolio_products = @portfolio.portfolio_products.joins(:product).order(:name)
     @possible_products = Product.order(:name) - @portfolio.products
   end
   
   
+  # Destroys given portfolio and redirects to its parent group.
+  def destroy
+    @group = Group.find(@portfolio.group_id)
+    @portfolio.destroy
+    flash[:notice] = t(:portfolio) + t(:deleted)
+    redirect_to edit_groups_path(@group)
+  end
+  
+  
+  # Adds new product to the portfolio if one is supplied, removes any which were marked for removal. 
   def update
+    unless params[:product].blank? || params[:product][:id].blank?
+      @portfolio.products << Product.find(params[:product][:id])
+      flash[:notice] = "Product added!"
+    end
     if @portfolio.update_attributes(params[:portfolio])
       flash[:notice] = "Yay!"
       redirect_to edit_portfolio_path(@portfolio.id)
@@ -22,6 +36,7 @@ class PortfoliosController < ApplicationController
   
   
   private 
+    # Loads the portfolio in question.
     def load_portfolio
       @portfolio = Portfolio.find(params[:id])
     end
