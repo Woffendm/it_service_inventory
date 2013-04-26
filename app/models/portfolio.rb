33b9fc6a7a5658d4cf1680be79a34f61e9f3a086
Michael Woffendin <woffendm@onid.orst.edu>
2013-04-25 18:44:29 -0700

    Fix error on portfolio update caused by validation

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,10 +7,17 @@ class Portfolio < ActiveRecord::Base
   has_many :products, :through => :portfolio_products
   accepts_nested_attributes_for :portfolio_products,  :allow_destroy => true
   delegate :name, :to => :portfolio_name, :allow_nil => true
+  validate :group_does_not_have_this_portfolio_multiple_times
   
-  validate :no_group_has_this_portfolio_multiple_times
   
-  def no_group_has_this_portfolio_multiple_times
-    errors.add :group_id, "This group already has a portfolio with this name" if Portfolio.where(:group_id => group_id, :portfolio_name_id => portfolio_name_id).first
+  # Checks to see if there is a portfolio with the same portfolio name and group as the one 
+  # being saved. Cancels save if there is one, and it is not the current portfolio being saved. 
+  # The second check is done for updating.
+  def group_does_not_have_this_portfolio_multiple_times
+    portfolio = Portfolio.where(:group_id => group_id, 
+                :portfolio_name_id => portfolio_name_id).first
+    if portfolio && self != portfolio
+      errors.add :group_id, "This group already has a portfolio with this name" 
+    end
   end
 end
