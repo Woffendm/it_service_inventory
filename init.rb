4952ac2346ce72b9e9704f2419bd08aee00cdca1
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2013-08-29 16:58:23 -0700

    Add scrum board option to top menu

--- init.rb
+++ init.rb
@@ -1,14 +1,14 @@
 require 'redmine'
 Redmine::Plugin.register :scrum2b do
   name 'Scrum2B Plugin'
-  author 'Scrum2B'
+  author 'Scrum2B, Michael Woffendin'
   description %Q{A scrum tool for team to work:
                   - Scrum board
                   - Customize views
                 }
-  version '0.1'
+  version '0.2'
   url 'https://github.com/scrum2b/scrum2b'
-  author_url 'http://scrum2b.com'
+
 
   settings :default => {
        'board_columns' => {
@@ -38,6 +38,9 @@ Redmine::Plugin.register :scrum2b do
     permission :view_issue, :s2b_boards => :index
   end
   menu :project_menu, :s2b_lists, { :controller => :s2b_lists, :action => :index }, :caption => :label_scrum2b, :after => :activity, :param => :project_id
- end
+  
+  menu :top_menu, :scrum_board, { :controller => :s2b_lists, :action => :index }, :caption => "Sprint Board"
+  
+end
 
 
