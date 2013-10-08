9d3dda20d076a958996b575a6bb38b9b3ef7fee7
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2013-10-08 12:46:03 -0700

    Update settings page to allow use of custom priority

--- init.rb
+++ init.rb
@@ -3,37 +3,43 @@ Redmine::Plugin.register :scrum2b do
   name 'Scrum2B Plugin'
   author 'Scrum2B, Michael Woffendin'
   description %Q{A scrum tool for team to work:
-                  - Scrum board
-                  - Customize views
+                  Scrum board
+                  Customize views
                 }
-  version '0.2'
+  version '1.0.1'
   url 'https://github.com/scrum2b/scrum2b'
 
 
   settings :default => {
-       'board_columns' => {
-         '0'=> {
-           'position' => '1', 'name' => 'Not Started', 'statuses' => {}
-         }, 
-         '1' => {
-           'position' => '2', 'name' => 'In Progress', 'statuses' => {}
-         }, 
-         '2' => {
-           'position' => '3', 'name' => 'Code Review', 'statuses' => {}
-         }, 
-         '3' => {
-           'position' => '4', 'name' => 'Resolved', 'statuses' => {}
-         }, 
-         '4' => {
-           'position' => '5', 'name' => 'Blocked', 'statuses' => {}
-         } 
-       }, 
-       'use_version_for_sprint' => 'true',
-       'custom_field_id' => '',
-       'use_default_priority' => 'true',
-       'current_sprint' => '',
-       'show_progress_bars' => 'true'
-     }, :partial => 'settings/scrum2b'
+    'board_columns' => {
+      '0'=> {
+        'position' => '1', 'name' => 'Not Started', 'statuses' => {}
+      }, 
+      '1' => {
+        'position' => '2', 'name' => 'In Progress', 'statuses' => {}
+      }, 
+      '2' => {
+        'position' => '3', 'name' => 'Code Review', 'statuses' => {}
+      }, 
+      '3' => {
+        'position' => '4', 'name' => 'Resolved', 'statuses' => {}
+      }, 
+      '4' => {
+        'position' => '5', 'name' => 'Blocked', 'statuses' => {}
+      } 
+    }, 
+    'sprint' => {
+      'use_default'      => 'true',
+      'custom_field_id'  => '',
+      'current_sprint'   => ''
+    },
+    'priority' => {
+      'use_default'      => 'true',
+      'custom_field_id'  => ''
+    },
+    'show_progress_bars' => 'true'
+  }, 
+  :partial => 'settings/scrum2b'
   
   project_module :scrum2b do
 
