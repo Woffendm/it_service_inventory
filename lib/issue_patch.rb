fda83dec95616b69a1b693b5bd9a5177e5e9ca8b
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2013-10-14 12:08:34 -0700

    Extend issue class to add method for getting specific custom value (get_custom_value)

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -17,11 +17,22 @@ module IssuePatch
   
   
   module InstanceMethods
-    # Returns the issue's custom field value for the designated custom field, if it has one.
+    # Returns the issue's custom field value object for the designated custom field, if it has one.
     def get_custom_field_value(custom_field)
       self.custom_field_values.each do |cfv|
         next unless cfv.custom_field_id == custom_field.id
         return cfv
+      end
+      return nil
+    end
+    
+    
+    # Returns the value of the custom field for the issue. Use this one if you are displaying data
+    # because you can include the custom_values in a query.
+    def get_custom_value(custom_field)
+      self.custom_values.each do |cv|
+        next unless cv.custom_field_id == custom_field.id
+        return cv.value
       end
       return nil
     end
