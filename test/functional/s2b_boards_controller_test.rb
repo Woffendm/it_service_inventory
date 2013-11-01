3873f4604dfc14bab6e250317ad7b4ba37bd5f75
Michael Phelps <michael.phelps@oregonstate.edu>
2013-11-01 13:27:51 -0700

    All the files are configured to properly be able to run tests. Also made a few basic tests that currently all fail

--- test/functional/s2b_boards_controller_test.rb
+++ test/functional/s2b_boards_controller_test.rb
@@ -14,36 +14,46 @@ class S2bBoardsControllerTest < ActionController::TestCase
 
 
     Setting.host_name = 'test.example.com'
+    
+    @issue = Issue.first
+    @issue_id = @issue.id
 
     @user = User.find_by_login("admin")
+    
+    @controller = S2bBoardsController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
   end
 
   def teardown
     reset_fixtures
   end
   
-  #def setup
+  test "copy pasta test" do
+    # log_user('admin', 'admin')   
+    @request.session[:user_id] = 1
+    Setting.default_language = 'en'
     
-  #  @controller = S2bBoardsController.new
-  # @request    = ActionController::TestRequest.new
-  #  @response   = ActionController::TestResponse.new
-  #  User.current = nil
-  #end
+    get :index
+    assert_response :success
+  end
   
   test "should get index" do
+    User.current = @user
+    get :index
+    assert_response :success
+    assert_template :index
+    assert_not_nil assigns(:projects.first.name)
+  end
+  
+  test "should get index issue one" do
     get :index
     assert_response :success
     assert_template :index
   end
   
-  test "should get index issue one" do
-    get :index, :issue_id => 1
-    assert_response :success
-    assert_template :index
-  end
-  
   test "should get update_status" do
-    get :update_status
+    get :update_status, :issue_id => @issue_id
     assert_response :success
     assert_template :update_status
   end
@@ -66,10 +76,22 @@ class S2bBoardsControllerTest < ActionController::TestCase
     assert_template :update
   end
   
-  test "should get filter_issues_onboard" do
-    get :filter_issues_onboard
-    assert_response :success
-    assert_template :filter_issues_onboard
+  test "should create new issue" do
+    
+    
+    post :create, :subject => "Stuff",
+                  :description => "letters!!!",
+                  :tracker_id => Tracker.first.id,
+                  :project_id => Project.first.id,
+                  :assigned_to_id => 3,
+                  :priority_id => 1,
+                  :fixed_version_id => 1, 
+                  :start_date => "2013-10-28",
+                  :due_date => "2013-11-5", 
+                  :estimated_hours => 5,
+                  :author_id => @user.id
+                  
+    assert_response :success 
   end
   
   
--- test/functional/s2b_lists_controller_test.rb
+++ test/functional/s2b_lists_controller_test.rb
@@ -2,7 +2,5 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class ListsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+  
 end
